03a902812b8330e65c53baf040bc06df5a7913c7
joao-tinelli <joao_tinelli@outlook.com>
2024-06-19 11:45:13 -0300

    [PARENT] fact parent (x,y) added.

---
+++ family_tree.pl
@@ -0,0 +1,56 @@
+%----------------------------------------------
+% The Corleone Family Tree
+%----------------------------------------------
+
+% Parent(X,Y) -: indicates that X is the parent of Y
+parent(signora, vito).
+parent(antonio, vito).
+parent(vito, michael).
+parent(vito, sonny).
+parent(vito, connie).
+parent(vito, fredo).
+parent(vito, tom).
+parent(carmella, michael).
+parent(carmella, sonny).
+parent(carmella, connie).
+parent(carmella, fredo).
+parent(carmella, tom).
+parent(michael, kay).
+parent(michael, apollonia).
+parent(michael, anthony).
+parent(michael, mary).
+parent(sonny, lucy).
+parent(sonny, sandra).
+parent(sonny, vincent).
+parent(sonny, santino_jr).
+parent(sonny, francessa).
+parent(sonny, frank).
+parent(connie, carlo).
+parent(connie, victor).
+parent(connie, michael_francis).
+parent(fredo, deanna).
+parent(tom, theresa).
+parent(tom, frank_).
+parent(tom, andrew).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
